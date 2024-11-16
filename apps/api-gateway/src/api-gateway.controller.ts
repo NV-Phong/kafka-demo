0b7d493bd21cf24d4e1400861b29fc32e13eee72
NV-Phong <0h9ng03@gmail.com>
2024-11-16 15:52:25 +0700

    [ref] send pattern

--- apps/api-gateway/src/api-gateway.controller.ts
+++ apps/api-gateway/src/api-gateway.controller.ts
@@ -1,55 +1,37 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Inject,
-  Param,
-  Post,
-} from '@nestjs/common';
-import { Client, Transport, ClientKafka } from '@nestjs/microservices';
+import { Body, Controller, Delete, Get, Inject, Param, Post } from '@nestjs/common';
+import { ApiGatewayService } from './api-gateway.service';
 
 @Controller()
 export class ApiGatewayController {
-  constructor(@Inject('KAFKA_CLIENT') private readonly client: ClientKafka) {}
-
-  async onModuleInit() {
-    this.client.subscribeToResponseOf('user_created');
-    this.client.subscribeToResponseOf('hello');
-    this.client.subscribeToResponseOf('User-Read');
-    this.client.subscribeToResponseOf('User-Create');
-    this.client.subscribeToResponseOf('User-Delete');
-    this.client.subscribeToResponseOf('User-Find');
-    await this.client.connect();
-  }
+  constructor(private readonly kafkaService: ApiGatewayService) {}
 
   @Get('username/:username')
   findUser(@Param('username') username: string) {
-    return this.client.send('User-Find', username);
+    return this.kafkaService.send('User-Find', username);
   }
 
   @Delete(':id')
   deleteUser(@Param('id') IDUser: string) {
-    return this.client.send('User-Delete', IDUser);
+    return this.kafkaService.send('User-Delete', IDUser);
   }
 
   @Post()
   createUser(@Body() user: any) {
-    return this.client.send('user_created', user);
+    return this.kafkaService.send('user_created', user);
   }
 
   @Post('create')
   createUsers(@Body() user: any) {
-    return this.client.send('User-Create', user);
+    return this.kafkaService.send('User-Create', user);
   }
 
   @Get('all')
   getAllUsers() {
-    return this.client.send('User-Read', {});
+    return this.kafkaService.send('User-Read', {});
   }
 
   @Get()
   hello() {
-    return this.client.send('hello', {});
+    return this.kafkaService.send('hello', {});
   }
 }
--- apps/api-gateway/src/api-gateway.service.ts
+++ apps/api-gateway/src/api-gateway.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
-export class ApiGatewayService {
-  getHello(): string {
-    return 'Hello World!';
+export class ApiGatewayService implements OnModuleInit {
+  constructor(@Inject('KAFKA_CLIENT') private readonly client: ClientKafka) {}
+
+  async onModuleInit() {
+    this.client.subscribeToResponseOf('user_created');
+    this.client.subscribeToResponseOf('hello');
+    this.client.subscribeToResponseOf('User-Read');
+    this.client.subscribeToResponseOf('User-Create');
+    this.client.subscribeToResponseOf('User-Delete');
+    this.client.subscribeToResponseOf('User-Find');
+    await this.client.connect();
+  }
+  send(pattern: string, data: any) {
+    return this.client.send(pattern, data);
   }
 }
